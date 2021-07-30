9679cb120a972c433b1ad09735acaefe74988ba2
Abduqodir1234 <gofurboyevabduqodir@gmail.com>
2021-07-30 18:28:45 +0500

    Abduqodir

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   images: {
-    domains: ["127.0.0.1",],
+    domains: ["uzmp3.herokuapp.com",],
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
