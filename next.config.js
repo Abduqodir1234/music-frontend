53f39058406cd90492c3a654d46c601f9a6ac0bc
root <root@DESKTOP-GNEQ3HU.localdomain>
2021-08-11 19:35:10 +0500

    A'
    '

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 module.exports = {
   images: {
-    domains: ["127.0.0.1", "api.wolt.uz"],
+    domains: ["uzmp3.herokuapp.com", "api.wolt.uz"],
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
